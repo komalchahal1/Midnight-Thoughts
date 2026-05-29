79e47a71eef5f25d6952eed1b3aac173cb5eb34d
komzy_codes <komalchahal1225@gmail.com>
2026-05-29 14:39:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -410,3 +410,22 @@ musicToggle.addEventListener("click", () => {
   }
 
 });
+
+// =========================
+// SERVICE WORKER
+// =========================
+
+if("serviceWorker" in navigator){
+
+  window.addEventListener("load", () => {
+
+    navigator.serviceWorker
+      .register("./sw.js")
+
+      .then(() => {
+        console.log("Service Worker Registered");
+      });
+
+  });
+
+}
